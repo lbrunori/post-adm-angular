47cb83c48de30164e3b3ee62a0971a1436e3a077
LBrunori <lucas.brunori@gmail.com>
2017-08-07 19:12:13 -0300

    main structure desingned

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,52 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms";
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { PublicacionesComponent } from './home/publicaciones/publicaciones.component';
+import { UsuariosComponent } from './home/usuarios/usuarios.component';
+import { AsideMenuComponent } from './home/aside-menu/aside-menu.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [{
+      path: '',
+      redirectTo: 'publicaciones',
+      pathMatch: 'full'
+    }, {
+      path: 'usuarios',
+      component: UsuariosComponent
+    }, {
+      path: 'publicaciones',
+      component: PublicacionesComponent
+    }]
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    HomeComponent,
+    PublicacionesComponent,
+    UsuariosComponent,
+    AsideMenuComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  iniciarSesion(inputEmail: HTMLInputElement, inputContrasenia: HTMLInputElement): void {
+    console.log(inputEmail.value);
+    console.log(inputContrasenia.value);
+  }
 }
